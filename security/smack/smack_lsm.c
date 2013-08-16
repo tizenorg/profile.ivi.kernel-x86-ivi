c22f5520db5a1ae00843f638ee101adee1314c07
Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
2013-08-16 10:21:02 +0300

    Revert "HACK: smack oops workaround"
    This reverts commit aadf905a7637585f9b89a574bc76fa9d073da781.
    
    This hack is not needed any longer, the problem was fixed in this commit:
    
    d6cbf35 cgroup: initialize xattr before calling d_instantiate()
    
    Signed-off-by: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -69,8 +69,7 @@ static struct smack_known *smk_fetch(const char *name, struct inode *ip,
 	char *buffer;
 	struct smack_known *skp = NULL;
 
-	if (ip->i_op->getxattr == NULL || dp->d_inode == NULL ||
-	    dp->d_fsdata == NULL)
+	if (ip->i_op->getxattr == NULL)
 		return NULL;
 
 	buffer = kzalloc(SMK_LONGLABEL, GFP_KERNEL);
